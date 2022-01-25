45d6c5352b8e0c5662f9c952278e476f252cc1db
Asus <molecula80@gmail.com>
2022-01-25 19:23:37 +0300

    find hotels by city name.

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from telebot import TeleBot
 from decouple import config
-from db_connection import db_table_val
 import requests
 import json
+from db_connection import db_table_val
 
 TOKEN = config('TOKEN')
 bot = TeleBot(TOKEN)
 
 x_rapid_api_key = config('X-RapidAPI-Key')
 
-url = "https://hotels4.p.rapidapi.com/properties/list"
+search_url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+properties_url = "https://hotels4.p.rapidapi.com/properties/list"
 
 headers = {
     'x-rapidapi-host': "hotels4.p.rapidapi.com",
     'x-rapidapi-key': x_rapid_api_key
     }
+
+
+def find_results(url, querystring):
+    """ Функция. Ищет результаты по url и строке запроса. """
+    response = requests.request("GET", url, headers=headers,
+                                params=querystring)
+    return json.loads(response.text)
 
 
 @bot.message_handler(commands=["start"])
@@ -37,21 +45,28 @@ def start(message) -> None:
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message) -> None:
     """ Функция. Реагирует на текстовые сообщения. """
-    querystring = {"destinationId": "1506246",
-                   "pageNumber": "1",
-                   "pageSize": "10",
-                   "checkIn": "2022-01-25",
-                   "checkOut": "2020-02-01",
-                   "adults1": "1",
-                   "sortOrder": "PRICE",
-                   "locale": "en_US",
-                   "currency": "USD"}
-    response = requests.request("GET", url, headers=headers,
-                                params=querystring)
-    results = json.loads(response.text)
+    # Ищем destination_id города Нью-Йорк.
+    search_querystring = {"query": "new york",
+                          "locale": "en_US",
+                          "currency": "USD"}
+    search_r = find_results(url=search_url, querystring=search_querystring)
+    destination_id = \
+        search_r["suggestions"][0]["entities"][0]["destinationId"]
+    # Ищем 10 самых дешевых отелей.
+    properties_querystring = {"destinationId": destination_id,
+                              "pageNumber": "1",
+                              "pageSize": "10",
+                              "checkIn": "2022-01-25",
+                              "checkOut": "2020-02-01",
+                              "adults1": "1",
+                              "sortOrder": "PRICE",
+                              "locale": "en_US",
+                              "currency": "USD"}
+    properties_r = find_results(url=properties_url,
+                                querystring=properties_querystring)
     if message.text == "/new-york":
-        answer = '\n'.join(hotel["name"] for hotel in results["data"]["body"][
-            "searchResults"]["results"])
+        answer = '\n'.join(hotel["name"] for hotel in properties_r["data"][
+            "body"]["searchResults"]["results"])
         bot.send_message(message.from_user.id, answer)
     else:
         bot.send_message(message.from_user.id, "Я не понимаю.")
